8c59f122578a74065b68560776de248a3c7d73f2
Jon Ryer <jonathan.ryer@gmail.com>
2018-10-30 15:23:18 -0500

    categories api route testing

--- server/api/categories.spec.js
+++ server/api/categories.spec.js
@@ -5,27 +5,22 @@ const db = require('../db')
 
 const app = require('../index')
 const Category = db.model('category')
-const sinon = require('sinon')
 const agent = require('supertest')(app)
 
 describe('Category Routers routes', () => {
-  const {findAll: categoryFindAll} = Category
-
   beforeEach(() => {
-    Category.findAll = sinon.spy(() => [
-      {id: 1, name: 'Shirt'},
-      {id: 2, name: 'Blue'}
-    ])
+    return db.sync({force: true})
   })
-  afterEach(() => {
-    Category.findAll = categoryFindAll
-  })
-  it('GET /api/categories responds with all categories', async () => {
-    const response = await agent.get('/api/categories').expect(200)
-    expect(response.body).to.deep.equal([
-      {id: 1, name: 'Shirt'},
-      {id: 2, name: 'Blue'}
-    ])
-    expect(Category.findAll.calledOnce).to.be.equal(true)
-  })
-}) // end describe('/api/users')
+
+  describe('/api/categories/', () => {
+    beforeEach(() => {
+      return Category.bulkCreate([{name: 'Shirt'}, {name: 'Red'}])
+    })
+    it('GET /api/categories', async () => {
+      const res = await agent.get('/api/categories').expect(200)
+      expect(res.body).to.be.an('array')
+      expect(res.body[0].name).to.be.equal('Shirt')
+      expect(res.body[1].name).to.be.equal('Red')
+    })
+  }) // end describe('/api/users')
+}) // end describe('User routes')
