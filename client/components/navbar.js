ea3b44523444937e118acda014b4369ca93aa11e
Paul Ebreo <paulebreo@triton.edu>
2018-10-31 10:16:37 -0500

    start materialize navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,27 +6,27 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
     <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/products">Products</Link>
-          <Link to="/categories">Categories</Link>
-        </div>
-      )}
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">
+          Logo
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <a href="sass.html">Login</a>
+          </li>
+          <li>
+            <a href="badges.html">Sign Up</a>
+          </li>
+          <li>
+            <a href="collapsible.html">Products</a>
+          </li>
+          <li>
+            <a href="collapsible.html">Categories</a>
+          </li>
+        </ul>
+      </div>
     </nav>
-    <hr />
   </div>
 )
 
