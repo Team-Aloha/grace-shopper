4de0190c3fde883bae437266db778785b331e2c4
sacreutz <sacreutz@gmail.com>
2018-11-01 11:55:36 -0500

    added route for product detail

--- client/components/products/ProductDetail.js
+++ client/components/products/ProductDetail.js
@@ -1,18 +1,50 @@
 import React from 'react'
+import {withRouter, Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {fetchOneProduct, postProductToCart} from '../../store'
+import store from '../../store'
 
-const SingleProductDetail = props => {
-  const {item} = props
+class ProductDetail extends React.Component {
+
+  componentDidMount() {
+    const productId = this.props.match.params.productId
+    this.props.getAProduct(productId)
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault()
+    const productId = this.props.match.params.productId
+    this.props.addProduct(productId)
+  }
+
+  render() {
+  const {product} = this.props
   return (
     <React.Fragment>
       <tr>
-        <td>{item.title}</td>
+        <td>{product.title}</td>
 
-        <td>{quantity}</td>
-        <td>{item.price}</td>
-        <td>Add Button Here</td>
+        <td>{product.quantity}</td>
+        <td>{product.price}</td>
+        <td onSubmit={this.handleSubmit}>Add to Cart</td>
       </tr>
     </React.Fragment>
   )
+  }
+
 }
 
-export default SingleProductDetail
+const mapStateToProps = state => {
+  return {
+    product: state.product
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  getAProduct: (productId) => dispatch(fetchOneProduct(productId)),
+  addProduct: (productId) => dispatch(postProductToCart(productId))
+})
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(ProductDetail)
+)
