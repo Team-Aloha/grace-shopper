930b48d92dcc77fd3359c6a596317233e66e4826
Jon Ryer <jonathan.ryer@gmail.com>
2018-11-01 09:35:00 -0500

    added more utility functions. started implementing cart specs

---
+++ server/api/cart.spec.js
@@ -0,0 +1,36 @@
+/* global describe beforeEach it */
+const {expect} = require('chai')
+const db = require('../db')
+
+const app = require('../index')
+// const Cart = db.model('cart')
+// const agent = require('supertest')(app)
+const {
+  createUserWithCart,
+  createUserWithNoCart
+} = require('../utils/createUsers')
+
+describe('Category API routes', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('/api/cart/', () => {
+    // beforeEach(() => {
+    //   return Category.bulkCreate([{name: 'Shirt'}, {name: 'Red'}])
+    // })
+    it('GET /api/cart for preexisting cart', async () => {
+      const user = await createUserWithCart('cart@cart.com')
+      const res = await user.get('/api/cart').expect(200)
+      expect(res.body.products).to.be.an('array')
+      expect(res.body.products.length).to.be.greaterThan(0)
+    })
+    // it.only('GET /api/cart for no cart creates and returns an empty cart', async () => {
+    //   const user = await createUserWithNoCart('nocart@cart.com')
+    //   const res = await user.get('/api/cart').expect(200)
+    //   console.log(res.body)
+    //   expect(res.body.products).to.be.an('array')
+    //   expect(res.body.products.length).to.be.equal(0)
+    // })
+  }) // end describe('/api/users')
+}) // end describe('User routes')
--- server/db/models/Cart.js
+++ server/db/models/Cart.js
@@ -4,11 +4,7 @@ const db = require('../db')
 const Cart = db.define('cart', {
   products: {
     type: Sequelize.ARRAY(Sequelize.JSONB)
-  },
-  total: {
-    type: Sequelize.INTEGER
   }
-
 })
 
 module.exports = Cart
