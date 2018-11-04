bf05d4996a3e9611a55dcb0cb36ac91604dd67f7
Jon Ryer <jonathan.ryer@gmail.com>
2018-11-04 16:06:21 -0600

    added helper function to get product name

--- client/components/user/OrderList.js
+++ client/components/user/OrderList.js
@@ -5,62 +5,67 @@ import {fetchOneProduct, updateProduct, fetchOrders} from '../../store'
 import {fetchCategories} from '../../store'
 import store from '../../store'
 
-
 const getTotalAmount = products => {
- let totalAmount = products.map((item, i) => {
+  let totalAmount = products
+    .map((item, i) => {
       const displayItem = item
       return displayItem.price * displayItem.quantity
-
-  })
-  .reduce((a, b) => a + b, 0)
+    })
+    .reduce((a, b) => a + b, 0)
   return `${totalAmount}`
 }
 
 const DUMMY_ORDERS = [
- {
-     id: 1,
-     createdAt: '1/1/2018',
-     status: 'shipped',
-     products: [
-        [{
-            id:1,
-            title: 'blue shirt',
-            price: 100,
-            quantity: 1,
-            imageUrl: 'defaultShirt.png'
-        }],
-        [{
-            id:2,
-            title: 'red shirt',
-            price:  200,
-            quantity:1,
-            imageUrl: 'defaultShirt.png'
-        }]
-     ]
- },
- {
-     id: 2,
-     createdAt: '1/1/2018',
-     status: 'pending',
-     products: [
-        [{
-            id:1,
-            title: 'pink shirt',
-            price: 500,
-            quantity: 1,
-            imageUrl: 'defaultShirt.png'
-        }],
-        [{
-            id:2,
-            title: 'yellow shirt',
-            price:  500,
-            quantity:2,
-            imageUrl: 'defaultShirt.png'
-        }]
-     ]
- }
-
-
+  {
+    id: 1,
+    createdAt: '1/1/2018',
+    status: 'shipped',
+    products: [
+      [
+        {
+          id: 1,
+          title: 'blue shirt',
+          price: 100,
+          quantity: 1,
+          imageUrl: 'defaultShirt.png'
+        }
+      ],
+      [
+        {
+          id: 2,
+          title: 'red shirt',
+          price: 200,
+          quantity: 1,
+          imageUrl: 'defaultShirt.png'
+        }
+      ]
+    ]
+  },
+  {
+    id: 2,
+    createdAt: '1/1/2018',
+    status: 'pending',
+    products: [
+      [
+        {
+          id: 1,
+          title: 'pink shirt',
+          price: 500,
+          quantity: 1,
+          imageUrl: 'defaultShirt.png'
+        }
+      ],
+      [
+        {
+          id: 2,
+          title: 'yellow shirt',
+          price: 500,
+          quantity: 2,
+          imageUrl: 'defaultShirt.png'
+        }
+      ]
+    ]
+  }
 ]
 
 export class OrderList extends React.Component {
@@ -70,11 +75,10 @@ export class OrderList extends React.Component {
   //     readOnly: true,
   //     orders: []
   //   }
-    // store.dispatch(fetchOneCampus(campusId))
-   // this.handleChange = this.handleChange.bind(this)
-    //this.handleSubmit = this.handleSubmit.bind(this)
-    //this.toggleEditProfile = this.toggleEditProfile.bind(this)
-
+  // store.dispatch(fetchOneCampus(campusId))
+  // this.handleChange = this.handleChange.bind(this)
+  //this.handleSubmit = this.handleSubmit.bind(this)
+  //this.toggleEditProfile = this.toggleEditProfile.bind(this)
 
   // componentDidUpdate(prevProps, prevState) {
   //   // After fetching redux has passed in the student as
@@ -93,6 +97,14 @@ export class OrderList extends React.Component {
     console.log(this.props, 'props')
   }
 
+  getProductName = id => {
+    const productInfo = this.props.products.filter(product => id === product.id)
+    if (productInfo[0]) {
+      return productInfo[0].title
+    }
+    return ''
+  }
+
   // handleChange(evt) {
   //   evt.preventDefault()
   //   const user = {[evt.target.name]: evt.target.value}
@@ -109,48 +121,43 @@ export class OrderList extends React.Component {
     if (Object.keys(this.props.user).length < 1) {
       return <div>User not logged in...</div>
     }
-
     return (
       <React.Fragment>
-        {' '}
         <div className="card">
-          <div className="card-content" id="order-card" >
-          <table>
-        <thead>
-          <tr>
-              <th>Date</th>
-              <th>Products</th>
-              <th>Total</th>
-              <th>Status</th>
-          </tr>
-        </thead>
-
-        <tbody>
-            {this.props.orders.map(order=>{
-                return (
-                    <tr>
-                        <td>
-                       {order.createdAt}
-                       </td>
-                       <td>
-                           {order.products.map(product=>(<p>{product.title}</p>)) }
-                        </td>
-                        <td>
-                            {getTotalAmount(order.products)}
-                            </td>
-                            <td>
-                                <Link to={`/orders/${order.id}`}>{order.status}</Link>
-                            </td>
+          <div className="card-content" id="order-card">
+            <table>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Products</th>
+                  <th>Total</th>
+                  <th>Status</th>
                 </tr>
-                )
-            })
+              </thead>
 
-            }
-
-
-        </tbody>
-      </table>
-
+              <tbody>
+                {this.props.orders.map(order => {
+                  return (
+                    <tr key={order.id}>
+                      <td>{order.createdAt}</td>
+                      <td>
+                        {order.products.map(product => (
+                          <p key={product.id}>
+                            <Link to={`/products/${product.id}`}>
+                              {this.getProductName(product.id)}
+                            </Link>
+                          </p>
+                        ))}
+                      </td>
+                      <td>{getTotalAmount(order.products)}</td>
+                      <td>
+                        <Link to={`/orders/${order.id}`}>{order.status}</Link>
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
           </div>
         </div>
       </React.Fragment>
@@ -159,19 +166,20 @@ export class OrderList extends React.Component {
 }
 
 const mapStateToProps = state => {
+  const {user, orders, products, cart} = state
   return {
-    user: state.user,
-    orders: state.orders,
-    products: state.products,
-    cart: state.cart
+    user,
+    orders,
+    products,
+    cart
   }
 }
 
 //put thunk to fetch orders on props
 
 const mapDispatchToProps = dispatch => ({
-   fetchOrders: () => dispatch(fetchOrders())
-  })
+  fetchOrders: () => dispatch(fetchOrders())
+})
 
 export default withRouter(
   connect(mapStateToProps, mapDispatchToProps)(OrderList)
