16f56f5cb5f970b18b458d6043001aaf4a079c55
Ryan Burke <rdburke2891@gmail.com>
2018-11-06 10:08:03 -0600

    made local pics

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {connect} from 'react-redux'
-
 class Home extends React.Component {
 
   render(){
@@ -8,7 +7,7 @@ class Home extends React.Component {
       <React.Fragment>
 
         <div class="parallax-container">
-      <div class="parallax"><img src="https://cdn.cnn.com/cnnnext/dam/assets/170606121056-hawaii---travel-destination---shutterstock-457528552.jpg"/></div>
+      <div class="parallax"><img src="./hawaii.jpg"/></div>
     </div>
     <div class="section yellow lighten-5 wood">
       <div class="row container">
@@ -17,7 +16,7 @@ class Home extends React.Component {
       </div>
     </div>
     <div class="parallax-container">
-      <div class="parallax"><img src="http://corkscrewjc.com/wp-content/uploads/2014/12/maui-hawaii-beach.jpg"/></div>
+      <div class="parallax"><img src="./beachChair.jpg"/></div>
     </div>
 
 
