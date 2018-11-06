1752650701a4f03b4d1a7c64490cdc5bd62e40fe
Paul Ebreo <paulebreo@triton.edu>
2018-11-06 13:15:10 -0600

    make admin navbar

--- client/components/admin/Dashboard.js
+++ client/components/admin/Dashboard.js
@@ -3,7 +3,28 @@ import {connect} from 'react-redux'
 import {Link, withRouter} from 'react-router-dom'
 
 const Dashboard = props => {
-  return <div>Dashboard here</div>
+  return (
+    <div className="container">
+      <div className="row">
+        <nav id="category-nav">
+          <div className="nav-wrapper">
+            <ul className="left hide-on-med-and-down">
+              <li>
+                <a className="white-text" href="">
+                  Admin Actions:
+                </a>
+              </li>
+              <li>
+                <a className="white-text" onClick={() => console.log('foo')}>
+                  Add Products
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  )
 }
 
 const mapState = state => {
